29dde36d8d7f108f3be45dfeaeda9ed5871be956
Jeff Ripke <jripke74@gmail.com>
2021-08-16 10:26:10 -0700

    add html to index.html and js to app.js

--- app.js
+++ app.js
@@ -1,57 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      counter: 0,
-      name: '',
-      lastname: '',
-      // fullname: '',
-    };
-  },
-  watch: {
-    counter(value) {
-      if (value > 50) {
-        const that = this;
-        setTimeout(function() {
-          that.counter = 0;
-        }, 2000)
-        this.counter = 0;
-      }
-    },
-  },
-  computed: {
-    fullname() {
-      if (this.name === '' || this.lastname === '') {
-        return '';
-      }
-      return this.name + ' ' + this.lastname;
-    },
-  },
-  methods: {
-    outputFullname() {
-      if (this.name === '') {
-        return '';
-      }
-      return this.name + ' ' + 'Scharz';
-    },
-    confirmInput() {
-      this.confirmedName = this.name;
-    },
-    submitForm() {
-      alert('Submitted');
-    },
-    setName(event, lastname) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-    },
-    resetInput() {
-      this.name = '';
-    },
+    return { goals: [] };
   },
 });
 
-app.mount('#events');
+app.mount('#user-goals');
