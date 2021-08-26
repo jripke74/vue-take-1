a9bd9d55a5dd7c1c6cf137dfb9ff98b43cfceb0a
Jeff Ripke <jripke74@gmail.com>
2021-08-26 11:42:06 -0700

    10 add App.vue and basic code

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      friends: [
+        {
+          id: 'Max',
+          name: 'Maximillion',
+          email: 'fict@email.com',
+        },
+        {
+          id: 'Jeff',
+          name: 'Maximillion',
+          email: 'fict@email.com',
+        },
+      ],
+    };
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
