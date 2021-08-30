e525d81789ad605f5f86799c9975e662d5d848d2
Jeff Ripke <jripke74@gmail.com>
2021-08-30 08:42:58 -0700

    08 add NewFriend.vue component

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h2>My Friends</h2>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -50,6 +51,16 @@ export default {
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
     },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name,
+        phone,
+        email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
+    },
   },
 };
 </script>
@@ -80,7 +91,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -104,10 +116,27 @@ header {
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
+
 #app button:hover,
 #app button:active {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ vue-app/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData" >
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="enteredEmail" />
+    </div>
+    <div>
+      <label>E-Mail</label>
+      <input type="email" v-model="eneredEmail" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-contact'],
+  data() {
+    return {
+      enteredName: '',
+      enteredPhone: '',
+      enteredEmail: '',
+    };
+  },
+  method: {
+    submitData() {
+      this.$emit(
+        'add-contact',
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail,
+      );
+    },
+  },
+};
+</script>
